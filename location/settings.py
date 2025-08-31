b8c3b05f1dd0ac7f1785189764d6929891ca8b5e
copalsteve <copalsteve@gmail.com>
2025-08-31 23:06:40 +0100

    permetre l'affichage des fichier sur onrender avec cloudinary

--- location/settings.py
+++ location/settings.py
@@ -154,8 +154,9 @@ else:
         'API_KEY': config('CLOUDINARY_API_KEY'),
         'API_SECRET': config('CLOUDINARY_API_SECRET'),
     }
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 LANGUAGES = [
